77d997d1af344e9be75558aefb2f642cca57d3f3
ihshah512 <optiimran@gmail.com>
2025-02-13 22:31:01 -0500

    first commit

---
+++ list.cpp
@@ -0,0 +1,130 @@
+#include "list.h"
+#include <stdexcept>
+
+LinkedList::LinkedList() // constructor
+    : m_head(nullptr)
+{
+}
+
+LinkedList::~LinkedList() // destructor
+{
+    while (!empty())
+        removeFront();
+}
+
+bool LinkedList::empty() const // is list empty?
+{
+    return m_head == nullptr;
+}
+
+const string &LinkedList::front() const
+{ // get front element
+    if (!empty())
+        return m_head->m_elem;
+    else
+        throw runtime_error("Error: Reading front of empty list!");
+}
+
+void LinkedList::addFront(const string &toInsert)
+{
+}
+
+void LinkedList::removeFront()
+{
+}
+
+void LinkedList::append(const string &toInsert)
+{
+
+    /*
+        1. Frist check is the list empty
+        2. Does list has only one node
+    */
+    Node *m_nodeToInsert = new Node;
+
+    m_nodeToInsert->m_next = nullptr;
+    m_nodeToInsert->m_elem = toInsert;
+
+    if (m_head == nullptr)
+    {
+        m_head = m_nodeToInsert;
+        return;
+    }
+
+    Node *temp = m_head;
+
+    while (temp->m_next != nullptr)
+    {
+
+        temp = temp->m_next;
+    }
+
+    temp->m_next = m_nodeToInsert;
+}
+
+void LinkedList::removeTail()
+{
+
+    // In case of empty list
+    if (m_head == nullptr)
+    {
+        throw runtime_error("No tail to remove list is empty");
+        return;
+    }
+
+    // In case of having one item in list head itself
+    if (m_head->m_next == nullptr)
+    {
+
+        delete m_head;
+        m_head = nullptr;
+        return;
+    }
+
+    //in case of having multiple nodes
+    Node *temp = m_head;
+    while (temp->m_next->m_next != nullptr)
+    {
+        temp = temp->m_next;
+    }
+    delete temp->m_next;
+    temp->m_next = nullptr;
+}
+
+void LinkedList::addInBetween(const string &e,
+                              const string &toInsert)
+{
+}
+
+void LinkedList::remove(const string &e)
+{
+}
+
+void LinkedList::printList()
+{
+    // to test this function:
+    // case: the list is empty
+    // case: the list is not empty
+    Node *temp = m_head;
+    cout << "The current list is:" << endl;
+    while (temp != nullptr)
+    {
+        cout << temp->m_elem << " => ";
+        temp = temp->m_next;
+    }
+    cout << "END" << endl;
+}
+
+void LinkedList::append(const string &toInsert)
+{
+    /************************
+     * To be implemented
+     * *********************/
+}
+
+void LinkedList::removeTail()
+{
+    /************************
+     * To be implemented
+     * *********************/
+}
